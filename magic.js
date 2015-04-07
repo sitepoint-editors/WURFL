ff950c8560655ae69e08a3e0680222da2b005ecf
ddragoti <ddragoti@users.noreply.github.com>
2015-04-07 17:13:41 +0200

    Create magic.js

---
+++ magic.js
@@ -0,0 +1,5 @@
+console.log(WURFL);
+
+$('#wurfl .is-mobile span').html(WURFL.is_mobile);
+$('#wurfl .form-factor span').html( WURFL.form_factor);
+$('#wurfl .device-name span').html( WURFL.complete_device_name);
