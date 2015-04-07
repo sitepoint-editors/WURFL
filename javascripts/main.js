1e5de06e5213d0d2f3b67bcb7a815584176ce9f3
ddragoti <ddragoti@users.noreply.github.com>
2015-04-07 17:29:35 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,6 @@
-console.log('This would be the main JS file.');
+console.log(WURFL);
+
+$('#wurfl .is-mobile span').html(WURFL.is_mobile);
+$('#wurfl .form-factor span').html( WURFL.form_factor);
+$('#wurfl .device-name span').html( WURFL.complete_device_name);
+
